1cd50b4717d3c25e83eeb629c7eee7f4b24d7142
Lautaro Gonzalez Ruiz <lautawork@gmail.com>
2023-05-08 17:56:12 -0300

    Barra de precios

--- productos.js
+++ productos.js
@@ -44,3 +44,52 @@ dropdowns.forEach((dropdown) => {
     });
   });
 });
+
+//-----------------------------------------------------------------------------------
+
+const dropdownsPrecios = document.querySelectorAll(".dropdownPrecios");
+
+// bucle a través de todos los elementos boton-1
+dropdownsPrecios.forEach((dropdownP) => {
+  //obtener elementos internos de cada .DROPDOWNS
+  const selectPrecios = dropdownP.querySelector(".selectPrecios");
+  const caretPrecios = dropdownP.querySelector(".caretPrecios");
+  const menuPrecios = dropdownP.querySelector(".menuPrecios");
+  const optionsPrecios = dropdownP.querySelectorAll(".menuPrecios li");
+  const selectedPrecios = dropdownP.querySelector(".selectedPrecios");
+
+  // usamos este método para que funcionen varios menús dropdown de la página
+
+  //agregar un evento de clic al elemento seleccionado
+  selectPrecios.addEventListener("click", () => {
+    //agregue el estilo de SELECCION en el que se hizo clic al elemento de SELECCION
+    selectPrecios.classList.toggle("selectPrecios-clicked");
+
+    //agregue la rotación al elemento de intercalación
+    caretPrecios.classList.toggle("caretPrecios-rotate");
+
+    //agregue el estilo abierto al elemento del MENU
+    menuPrecios.classList.toggle("menuPrecios-open");
+  });
+
+  //bucle a través de todos los elementos de opción
+  optionsPrecios.forEach((optionP) => {
+    //agregue un evento de clic al elemento de OPTION
+    optionP.addEventListener("click", () => {
+      //Cambie la selección interna al texto interno de la opción en la que se hizo clic
+      selectedPrecios.innerText = optionP.innerText;
+      //cambiar seleccionar dentro del texto interior de la opción en la que se hizo clic
+      selectPrecios.classList.remove("selectPrecios-clicked");
+      //agregar estilo de rotación al elemento de intercalación
+      caretPrecios.classList.remove("caretPrecios-rotate");
+      //agregue el estilo abierto al elemento del MENU
+      menuPrecios.classList.remove("menuPrecios-open");
+      //eliminar la clase activa de todos los elementos de opción
+      optionsPrecios.forEach((optionP) => {
+        optionP.classList.remove("active");
+      });
+      //agregar clase activa al elemento de opción en el que se hizo clic
+      optionP.classList.add("active");
+    });
+  });
+});
