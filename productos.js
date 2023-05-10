10cd505fb4d1bbec95312b922864c8c6e87925a0
Lautaro Gonzalez Ruiz <lautawork@gmail.com>
2023-05-10 17:20:12 -0300

    Agregue boton de usuarios y detalles de estilos nav-prueba

--- productos.js
+++ productos.js
@@ -1,27 +1,78 @@
 //Javascript de Botones Marcas-Producto
 
+const dropdownsCuentas = document.querySelectorAll(".dropdownCuentas");
+
+// Bucle a través de todos los elementos .DROPDOWNS
+dropdownsCuentas.forEach((dropdownC) => {
+  //Obtener elementos internos de .DROPDOWNS
+  const selectCuentas = dropdownC.querySelector(".selectCuentas");
+  const caretCuentas = dropdownC.querySelector(".caretCuentas");
+  const menuCuentas = dropdownC.querySelector(".menuCuentas");
+  const optionsCuentas = dropdownC.querySelectorAll(".menuCuentas li");
+  const selectedCuentas = dropdownC.querySelector(".selectedCuentas");
+
+  //Método para que funcionen varios menús dropdown de la página
+
+  //Agregar un evento de clic al elemento seleccionado
+  selectCuentas.addEventListener("click", () => {
+    //Agregar el estilo de SELECCION en el que se hizo clic al elemento de SELECCION
+    selectCuentas.classList.toggle("select-clickedCuentas");
+
+    //Agrega la rotación al elemento de intercalación
+    caretCuentas.classList.toggle("caret-rotateCuentas");
+
+    //Agregar el estilo abierto al elemento del MENU
+    menuCuentas.classList.toggle("menu-openCuentas");
+  });
+
+  //bucle a través de todos los elementos de opción
+  optionsCuentas.forEach((optionC) => {
+    //agregue un evento de clic al elemento de OPTION
+    optionC.addEventListener("click", () => {
+      //Cambie la selección interna al texto interno de la opción en la que se hizo clic
+      selectedCuentas.innerText = optionC.innerText;
+      //cambiar seleccionar dentro del texto interior de la opción en la que se hizo clic
+      selectCuentas.classList.remove("select-clickedCuentas");
+      //agregar estilo de rotación al elemento de intercalación
+      caretCuentas.classList.remove("caret-rotateCuentas");
+      //agregue el estilo abierto al elemento del MENU
+      menuCuentas.classList.remove("menu-openCuentas");
+      //eliminar la clase activa de todos los elementos de opción
+      optionsCuentas.forEach((optionC) => {
+        optionC.classList.remove("active");
+      });
+      //agregar clase activa al elemento de opción en el que se hizo clic
+      optionC.classList.add("active");
+    });
+  });
+});
+
+//-----------------------------------------------------------------------------------
+
+//Javascript de Botones Marcas-Producto
+
 const dropdowns = document.querySelectorAll(".dropdown");
 
-// bucle a través de todos los elementos boton-1
+// Bucle a través de todos los elementos .DROPDOWNS
 dropdowns.forEach((dropdown) => {
-  //obtener elementos internos de cada .DROPDOWNS
+  //Obtener elementos internos de .DROPDOWNS
   const select = dropdown.querySelector(".select");
   const caret = dropdown.querySelector(".caret");
   const menu = dropdown.querySelector(".menu");
   const options = dropdown.querySelectorAll(".menu li");
   const selected = dropdown.querySelector(".selected");
 
-  // usamos este método para que funcionen varios menús dropdown de la página
+  //Método para que funcionen varios menús dropdown de la página
 
-  //agregar un evento de clic al elemento seleccionado
+  //Agregar un evento de clic al elemento seleccionado
   select.addEventListener("click", () => {
-    //agregue el estilo de SELECCION en el que se hizo clic al elemento de SELECCION
+    //Agregar el estilo de SELECCION en el que se hizo clic al elemento de SELECCION
     select.classList.toggle("select-clicked");
 
-    //agregue la rotación al elemento de intercalación
+    //Agrega la rotación al elemento de intercalación
     caret.classList.toggle("caret-rotate");
 
-    //agregue el estilo abierto al elemento del MENU
+    //Agregar el estilo abierto al elemento del MENU
     menu.classList.toggle("menu-open");
   });
 
@@ -99,4 +150,3 @@ dropdownsPrecios.forEach((dropdownP) => {
 });
 
 //-----------------------------------------------------------------------------------
-
